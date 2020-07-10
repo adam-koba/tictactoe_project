b448fd6f0ae7aee223da3a4612bae213b41f2b01
Adam Koba <adamkoba@icloud.com>
2020-07-10 21:32:58 +0200

    fixed some typos and add new functionality
    Typos were fixed and insertLetter function was renamed to insertSign. Working on playerMove() function and main() funcion

--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,7 @@
 # TicTacToe
+board = [ ' ' for x in range(10)]
 
-# board
-# display board
-# play game
-
-board = [' ' for x in range(10)]
-
-def insertLetter(sign, pos):
+def insertSign(sign, pos):
     global board
     board[pos] = sign
 
@@ -27,12 +22,66 @@ def printBoard(board):
 # This function return True if player win
 # sg is for sign, that means it is X or O
 def isWinner(bo, sg):
-    return (bo[1] == sg and bo[2] == sg and bo[3] == sg) or # across the top row
+    return ((bo[1] == sg and bo[2] == sg and bo[3] == sg) or # across the top row
     (bo[4] == sg and bo[5] == sg and bo[6] == sg) or # across the middle row
     (bo[7] == sg and bo[8] == sg and bo[9] == sg) or # across the bottom row
     (bo[1] == sg and bo[5] == sg and bo[9] == sg) or # diagonal
-    (bo[7] == ag and bo[5] == sg and bo[3] == sg) or # diagonal
+    (bo[7] == sg and bo[5] == sg and bo[3] == sg) or # diagonal
     (bo[1] == sg and bo[4] == sg and bo[7] == sg) or # down the left side
     (bo[2] == sg and bo[5] == sg and bo[8] == sg) or # down the middle
-    (bo[3] == sg and bo[6] == sg and bo[9] == sg) # down the right side
+    (bo[3] == sg and bo[6] == sg and bo[9] == sg)) # down the right side
+
+# choosing position to put X
+def playerMove():
+    run = True
+    while run:
+        move = input('Please select a position to place \'X\' (1-9): ')
+        try:
+            move = int(move)
+            if move > 0 and move < 10:
+                if spaceIsFree(move):
+                    run = False
+                    insertSign('X', move)
+                else:
+                    print('Sorry, this space is occupied!')
+            else:
+                print('Type a number within the range!')
+        except:
+            print('Please print a number!')
+#
+def compMove():
+    pass
+
+#
+def selectRandom():
+    pass
+
+# checking if the board is full
+def isBoardFull():
+    if board.count(' ') > 1:
+        return True
+    else:
+        return False
+#
+def main():
+    print('TicTacToe Game. The board has position from 1 to 9 starting at the top left. To win you have to complete line of your sign (diagonal, horizontal or vertical)')
+    printBoard()
+
+    while not(isBoardFull(board)):
+        if not(isWinner(board, 'O')):
+            playerMove()
+            printBoard()
+        else:
+            print('Sorry, opponent won this time ...')
+            break
+        if not(isWinner(board, 'X')):
+            compMove()
+            printBoard()
+        else:
+            print('YOU WON!')
+            break
+
+
+    if isBoardFull(board):
+        print('Tie Game!')
 
